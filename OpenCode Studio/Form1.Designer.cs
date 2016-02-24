498414a9b76af92b3ec01f8b47a85231c8fd4c12
brainadam24 <worldspace@live.de>
2016-02-24 13:44:56 +0100

    Initial commit on development tree

---
+++ OpenCode Studio/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace OpenCode_Studio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Berichte = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dateiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.neuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.öffnenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.speichernToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.projektToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kompilierenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ausführenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kompilierenUndAusführenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eingabeaufforderungToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.taschenrechnerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hilfeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dokumentationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.überToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.versionsinformationenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Quelltext = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.mainProgress = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Berichte);
+            this.groupBox1.Location = new System.Drawing.Point(12, 309);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(586, 131);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Berichte";
+            // 
+            // Berichte
+            // 
+            this.Berichte.Location = new System.Drawing.Point(6, 19);
+            this.Berichte.Name = "Berichte";
+            this.Berichte.ReadOnly = true;
+            this.Berichte.Size = new System.Drawing.Size(574, 106);
+            this.Berichte.TabIndex = 0;
+            this.Berichte.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dateiToolStripMenuItem,
+            this.projektToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.hilfeToolStripMenuItem,
+            this.überToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(910, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dateiToolStripMenuItem
+            // 
+            this.dateiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.neuToolStripMenuItem,
+            this.öffnenToolStripMenuItem,
+            this.speichernToolStripMenuItem});
+            this.dateiToolStripMenuItem.Name = "dateiToolStripMenuItem";
+            this.dateiToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.dateiToolStripMenuItem.Text = "Datei";
+            // 
+            // neuToolStripMenuItem
+            // 
+            this.neuToolStripMenuItem.Name = "neuToolStripMenuItem";
+            this.neuToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.neuToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.neuToolStripMenuItem.Text = "Neu";
+            // 
+            // öffnenToolStripMenuItem
+            // 
+            this.öffnenToolStripMenuItem.Name = "öffnenToolStripMenuItem";
+            this.öffnenToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.öffnenToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.öffnenToolStripMenuItem.Text = "Öffnen";
+            // 
+            // speichernToolStripMenuItem
+            // 
+            this.speichernToolStripMenuItem.Name = "speichernToolStripMenuItem";
+            this.speichernToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.speichernToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.speichernToolStripMenuItem.Text = "Speichern";
+            // 
+            // projektToolStripMenuItem
+            // 
+            this.projektToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kompilierenToolStripMenuItem,
+            this.ausführenToolStripMenuItem,
+            this.kompilierenUndAusführenToolStripMenuItem});
+            this.projektToolStripMenuItem.Name = "projektToolStripMenuItem";
+            this.projektToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.projektToolStripMenuItem.Text = "Projekt";
+            // 
+            // kompilierenToolStripMenuItem
+            // 
+            this.kompilierenToolStripMenuItem.Name = "kompilierenToolStripMenuItem";
+            this.kompilierenToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F3;
+            this.kompilierenToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.kompilierenToolStripMenuItem.Text = "Kompilieren";
+            // 
+            // ausführenToolStripMenuItem
+            // 
+            this.ausführenToolStripMenuItem.Name = "ausführenToolStripMenuItem";
+            this.ausführenToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F4;
+            this.ausführenToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.ausführenToolStripMenuItem.Text = "Ausführen";
+            this.ausführenToolStripMenuItem.Click += new System.EventHandler(this.ausführenToolStripMenuItem_Click);
+            // 
+            // kompilierenUndAusführenToolStripMenuItem
+            // 
+            this.kompilierenUndAusführenToolStripMenuItem.Name = "kompilierenUndAusführenToolStripMenuItem";
+            this.kompilierenUndAusführenToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F5;
+            this.kompilierenUndAusführenToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.kompilierenUndAusführenToolStripMenuItem.Text = "Kompilieren und Ausführen";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eingabeaufforderungToolStripMenuItem,
+            this.taschenrechnerToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // eingabeaufforderungToolStripMenuItem
+            // 
+            this.eingabeaufforderungToolStripMenuItem.Name = "eingabeaufforderungToolStripMenuItem";
+            this.eingabeaufforderungToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.eingabeaufforderungToolStripMenuItem.Text = "Eingabeaufforderung";
+            this.eingabeaufforderungToolStripMenuItem.Click += new System.EventHandler(this.eingabeaufforderungToolStripMenuItem_Click);
+            // 
+            // taschenrechnerToolStripMenuItem
+            // 
+            this.taschenrechnerToolStripMenuItem.Name = "taschenrechnerToolStripMenuItem";
+            this.taschenrechnerToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.taschenrechnerToolStripMenuItem.Text = "Taschenrechner";
+            this.taschenrechnerToolStripMenuItem.Click += new System.EventHandler(this.taschenrechnerToolStripMenuItem_Click);
+            // 
+            // hilfeToolStripMenuItem
+            // 
+            this.hilfeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dokumentationToolStripMenuItem});
+            this.hilfeToolStripMenuItem.Name = "hilfeToolStripMenuItem";
+            this.hilfeToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.hilfeToolStripMenuItem.Text = "Hilfe";
+            // 
+            // dokumentationToolStripMenuItem
+            // 
+            this.dokumentationToolStripMenuItem.Name = "dokumentationToolStripMenuItem";
+            this.dokumentationToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.dokumentationToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.dokumentationToolStripMenuItem.Text = "Dokumentation";
+            // 
+            // überToolStripMenuItem
+            // 
+            this.überToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.versionsinformationenToolStripMenuItem});
+            this.überToolStripMenuItem.Name = "überToolStripMenuItem";
+            this.überToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.überToolStripMenuItem.Text = "Über";
+            // 
+            // versionsinformationenToolStripMenuItem
+            // 
+            this.versionsinformationenToolStripMenuItem.Name = "versionsinformationenToolStripMenuItem";
+            this.versionsinformationenToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.versionsinformationenToolStripMenuItem.Text = "Versionsinformationen";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Quelltext);
+            this.groupBox2.Location = new System.Drawing.Point(12, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(586, 276);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Quelltext";
+            // 
+            // Quelltext
+            // 
+            this.Quelltext.Location = new System.Drawing.Point(6, 19);
+            this.Quelltext.Multiline = true;
+            this.Quelltext.Name = "Quelltext";
+            this.Quelltext.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.Quelltext.Size = new System.Drawing.Size(574, 251);
+            this.Quelltext.TabIndex = 0;
+            this.Quelltext.Text = "Ausgabe(\"Test1234\"); //Testkommentar\r\n$string = Eingabe(\"Bitte etwas eingeben\") ;" +
+    "\r\n/* multiline\r\n    comment\r\n*/";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(701, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mainProgress
+            // 
+            this.mainProgress.Location = new System.Drawing.Point(0, 481);
+            this.mainProgress.Name = "mainProgress";
+            this.mainProgress.Size = new System.Drawing.Size(910, 12);
+            this.mainProgress.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(910, 505);
+            this.Controls.Add(this.mainProgress);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.HelpButton = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "OpenCode Studio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox Berichte;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dateiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem öffnenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem speichernToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem projektToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kompilierenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ausführenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hilfeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem überToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem versionsinformationenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem taschenrechnerToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox Quelltext;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar mainProgress;
+        private System.Windows.Forms.ToolStripMenuItem kompilierenUndAusführenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem neuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dokumentationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eingabeaufforderungToolStripMenuItem;
+    }
+}
+
---
+++ OpenCode Studio/Form1.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace OpenCode_Studio
+{
+    public partial class Form1 : Form
+    {
+        string projektname = "unbekannt";
+        Dictionary<string, string> fcss = new Dictionary<string, string>();
+        Dictionary<string, string> lang = new Dictionary<string, string>();
+        public Form1()
+        {
+            //projektname = "search for file";
+
+            fcss["version"] = "0.0.0.1";
+            fcss["name"] = "OpenCode Studio";
+            fcss["language_name"] = "Opencode";
+
+            lang["in_version"] = "in der Version"; //später auslagern
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AlignItems();
+            //Report
+            Ausgabe(fcss["name"] + " " + fcss["version"] + " " + lang["in_version"] + " " + " wurde erfolgreich geladen.", 2, Color.Green);
+        }
+
+        private void AlignItems()
+        {
+            mainProgress.Location = new Point(0, Form1.ActiveForm.Height);// - mainProgress.Height);
+        }
+
+        private void Ausgabe(string p, int umbruch = 1, Color? schriftfarbe = null)
+        {
+            if (umbruch == 1) {
+                p = "\n" + p;
+            }
+            //Berichte.ForeColor = schriftfarbe.HasValue ? schriftfarbe.Value : Color.Black;
+            //Berichte.AppendText(p);
+            int start = Berichte.TextLength;
+            Berichte.AppendText(p);
+            int end = Berichte.TextLength;
+
+            // Textbox may transform chars, so (end-start) != text.Length
+            Berichte.Select(start, end - start);
+            {
+                Berichte.SelectionColor = schriftfarbe.HasValue ? schriftfarbe.Value : Color.Black;
+                // could set box.SelectionBackColor, box.SelectionFont too.
+            }
+            Berichte.SelectionLength = 0; // clear
+            Berichte.SelectionStart = Berichte.Text.Length; //Set the current caret position at the end
+            Berichte.ScrollToCaret();
+        }
+
+        private void ausführenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Starten();
+        }
+
+        private void Starten()
+        {
+            mainProgress.Value = 0;
+            //Array Befehle[];
+            //Befehle[1] = "";
+            Ausgabe("Projekt " + projektname + " wird gestartet.");
+            mainProgress.Value = 10;
+            string source = Quelltext.Text;
+            Ausgabe("Quelltext eingelesen");
+            mainProgress.Value = 20;
+
+            Ausgabe(source);
+            string compiled = Compile(source);
+            Ausgabe(compiled);
+            mainProgress.Value = 90;
+
+            Run(compiled);
+            mainProgress.Value = 100;
+        }
+
+        private string Compile(string input)
+        {
+            Ausgabe("kompiliere Quelltext...");
+
+            /*Keep it SHORT*/
+            //einzeilige Kommentare entfernen
+            input = Regex.Replace(input, @"//.*", "");
+            //mehrzeilige Kommentare entfernen
+            input = Regex.Replace(input, @"\/\*((.|\n)*?)\*\/", "");
+            //Umbrüche entfernen
+            input = Regex.Replace(input, @"\t|\n|\r", "");
+            //Leerzeichen entfernen(contraproductive)
+            //input = Regex.Replace(input, @" ", "");
+            Ausgabe("Source Code successfully minified");
+
+            /*And SIMPLE*/
+
+            Ausgabe("Source Code successfully compiled");
+            return input;
+        }
+
+        private void Run(string program)
+        {
+            Ausgabe("Programm wird gestartet...");
+            Ausgabe("--------------------------");
+            int length = program.Length;
+            /*Später auskommentieren*/Ausgabe("Quellcodelänge: " + length.ToString());
+            for (int i = 0; i == (length - 1); i++)
+            {
+                string sign = program.Substring(i, 1);
+                Ausgabe(sign);
+                if (sign == ";") {
+                    Ausgabe("Semikolon(Befehlsende) gefunden");
+                }
+            }
+            Ausgabe("--------------------------");
+            Ausgabe("Programmausführung beendet!");
+            GetVersionFromRegistry();
+        }
+
+        private static void GetVersionFromRegistry()
+        {
+            // Opens the registry key for the .NET Framework entry.
+            using (RegistryKey ndpKey =
+                RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, "").
+                OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+            {
+                // As an alternative, if you know the computers you will query are running .NET Framework 4.5 
+                // or later, you can use:
+                // using (RegistryKey ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, 
+                // RegistryView.Registry32).OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+                foreach (string versionKeyName in ndpKey.GetSubKeyNames())
+                {
+                    if (versionKeyName.StartsWith("v"))
+                    {
+
+                        RegistryKey versionKey = ndpKey.OpenSubKey(versionKeyName);
+                        string name = (string)versionKey.GetValue("Version", "");
+                        string sp = versionKey.GetValue("SP", "").ToString();
+                        string install = versionKey.GetValue("Install", "").ToString();
+                        if (install == "") //no install info, must be later.
+                            Console.WriteLine(versionKeyName + "  " + name);
+                        else
+                        {
+                            if (sp != "" && install == "1")
+                            {
+                                Console.WriteLine(versionKeyName + "  " + name + "  SP" + sp);
+                            }
+
+                        }
+                        if (name != "")
+                        {
+                            continue;
+                        }
+                        foreach (string subKeyName in versionKey.GetSubKeyNames())
+                        {
+                            RegistryKey subKey = versionKey.OpenSubKey(subKeyName);
+                            name = (string)subKey.GetValue("Version", "");
+                            if (name != "")
+                                sp = subKey.GetValue("SP", "").ToString();
+                            install = subKey.GetValue("Install", "").ToString();
+                            if (install == "") //no install info, must be later.
+                                Console.WriteLine(versionKeyName + "  " + name);
+                            else
+                            {
+                                if (sp != "" && install == "1")
+                                {
+                                    Console.WriteLine("  " + subKeyName + "  " + name + "  SP" + sp);
+                                }
+                                else if (install == "1")
+                                {
+                                    Console.WriteLine("  " + subKeyName + "  " + name);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void taschenrechnerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("calc");
+            //System.Diagnostics.Process p = System.Diagnostics.Process.Start("calc.exe");
+            //p.WaitForInputIdle();
+            //NativeMethods.SetParent(p.MainWindowHandle, this.Handle);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Starten();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            AlignItems();
+        }
+
+        private void eingabeaufforderungToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("cmd");
+        }
+    }
+}
